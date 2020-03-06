7a6dac220e07a04a582f08091f710fd8be3e6c13
gipark <gipark1993@gmail.com>
2020-03-06 21:29:22 +0900

    study 외부모듈사용하기

---
+++ Express/01. Arraylist/main.js
@@ -0,0 +1,15 @@
+/**
+ * Arraylist
+ */
+var Arraylist = require("arraylist");
+
+var list = new Arraylist;
+
+list.add(100);
+list.add(200);
+
+var a1 = list.get(0);
+var a2 = list.get(1);
+
+console.log("a1 : ", a1);
+console.log("a2 : ", a2);
